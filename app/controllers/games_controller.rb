f45cebf4d56de11f8e622ec3e3ff4fc20e2133b1
Kevin Huang <yellow.khaos@gmail.com>
2018-12-17 13:49:06 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,27 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    @game = Game.new(game_params)
+    render json: @game, status: 201
+  end
 
+  def update
+    @game = Game.update(game_params)
+    render json: @game, status: 201
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: 201
+  end
+
+  def index
+    @games = Game.all
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
 end

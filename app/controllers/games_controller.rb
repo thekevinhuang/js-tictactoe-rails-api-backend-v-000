ff28781938d293e86947d828597ba2475fd0cef0
Kevin Huang <yellow.khaos@gmail.com>
2018-12-17 14:16:14 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
+    binding.pry
     @game = Game.create(params[:state])
     render json: @game, status: 201
   end
